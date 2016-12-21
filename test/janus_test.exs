a67530bdcca9a4f72a9ab15413684d28b8df8ca2
Nolan Darilek <nolan@thewordnerd.info>
2016-12-21 10:10:15 -0600

    Set up Bypass in tests.

--- test/janus_test.exs
+++ test/janus_test.exs
@@ -2,6 +2,11 @@ defmodule JanusTest do
   use ExUnit.Case
   doctest Janus
 
+  setup do
+    bypass = Bypass.open
+    {:ok, bypass: bypass}
+  end
+
   test "the truth" do
     assert 1 + 1 == 2
   end
