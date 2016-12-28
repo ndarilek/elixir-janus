49477fc70c51f8377afcc19623dc8df0f63357e7
Nolan Darilek <nolan@thewordnerd.info>
2016-12-28 09:48:45 -0600

    Add basic documentation for Janus module.

--- lib/janus.ex
+++ lib/janus.ex
@@ -2,6 +2,14 @@ import Janus.Util
 
 defmodule Janus do
 
+  @moduledoc """
+  This library is a client for the [Janus REST API](https://janus.conf.meetecho.com/docs/rest.html).
+  """
+
+  @doc """
+  Retrieves details on the Janus server located at `url`
+  """
+
   def info(url), do: get("#{url}/info")
 
 end
