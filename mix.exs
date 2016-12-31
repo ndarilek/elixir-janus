f2f3cff74914ccd3bf38548b4470e1a7a821ee5a
Nolan Darilek <nolan@thewordnerd.info>
2016-12-31 12:31:33 -0600

    Integrate ExDoc.

--- mix.exs
+++ mix.exs
@@ -2,12 +2,21 @@ defmodule Janus.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :janus,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :janus,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      name: "Janus",
+      source_url: "https://github.com/ndarilek/elixir-janus",
+      homepage_url: "https://github.com/ndarilek/elixir-janus",
+      docs: [
+        main: "Janus",
+        extras: ["README.md"]
+      ]
+    ]
   end
 
   # Configuration for the OTP application
@@ -30,7 +39,9 @@ defmodule Janus.Mixfile do
     [
       {:httpoison, "~> 0.10.0"},
       {:poison, "~> 3.0"},
+      {:ex_doc, "~> 0.14", only: :dev},
       {:bypass, "~> 0.1", only: :test}
     ]
   end
+
 end
